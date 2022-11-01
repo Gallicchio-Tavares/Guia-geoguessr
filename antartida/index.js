a24446dc9713e5a4500cd93cdc0d5ae873a15091
Bianca Gallicchio <94980037+BiancaGallicchio@users.noreply.github.com>
2022-11-01 17:53:55 -0300

    Update index.js

--- antartida/index.js
+++ antartida/index.js
@@ -20,13 +20,13 @@ function topFunction() {
 
 /* Abaixo, pra fazer as imagens funcionarem */
 
-var slideIndex = [1,1,1,1];
-var slideId = ["mySlides1", "mySlides2", "mySlides3", "mySlides4"]
+var slideIndex = [1,1,1,1,1];
+var slideId = ["mySlides1", "mySlides2", "mySlides3", "mySlides4", "mySlides5"]
 showSlides(1, 0);
 showSlides(1, 1);
 showSlides(1, 2);
 showSlides(1, 3);
-// showSlides(1, 4);
+showSlides(1, 4);
 
 function plusSlides(n, no) {
   showSlides(slideIndex[no] += n, no);
