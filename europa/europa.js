6b5bedbae2f8ce04ffcb245493531e8e019f0390
Bianca Gallicchio <94980037+BiancaGallicchio@users.noreply.github.com>
2022-11-05 17:23:03 -0300

    Add files via upload

---
+++ europa/europa.js
@@ -0,0 +1,23 @@
+// create references to the modal...
+var modal = document.getElementById('myModal');
+// to all images -- note I'm using a class!
+var images = document.getElementsByClassName('myImages');
+// the image in the modal
+var modalImg = document.getElementById("img01");
+
+// Go through all of the images with our custom class
+for (var i = 0; i < images.length; i++) {
+  var img = images[i];
+  // and attach our click listener for this image.
+  img.onclick = function(evt) {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+}
+
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
