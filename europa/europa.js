301be43aa34940e0eaf8c23a493ddcd2de64aebd
Bianca Gallicchio <94980037+BiancaGallicchio@users.noreply.github.com>
2022-11-06 03:59:59 -0300

    Update europa.js

--- europa/europa.js
+++ europa/europa.js
@@ -21,3 +21,28 @@ var span = document.getElementsByClassName("close")[0];
 span.onclick = function() {
   modal.style.display = "none";
 }
+
+var slideIndex = [1];
+var slideId = ["mySlides1"]
+showSlides(1, 0);
+// showSlides(1, 1);
+// showSlides(1, 2);
+// showSlides(1, 3);
+// showSlides(1, 4);
+
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
+}
+function currentSlide(n, no) {
+  showSlides(slideIndex[no] = n, no);
+}
+function showSlides(n, no) {
+  var i;
+  var x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {slideIndex[no] = 1}    
+  if (n < 1) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  x[slideIndex[no]-1].style.display = "block";  
+}
